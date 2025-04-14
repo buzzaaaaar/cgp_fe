13b4bcb91d8563782e8706a316a965b890da81cc
Minidu <sbminidu@gmail.com>
2025-04-15 04:58:09 +0530

    SEOMetaTools

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        hanken: ['"Hanken Grotesk"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
