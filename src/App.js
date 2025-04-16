856b1eb1213aad98199906e3f2bc1d2f5f55eafd
Minidu <sbminidu@gmail.com>
2025-04-17 03:37:19 +0530

    FolderPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MetaTitleTool from './pages/MetaTitleTool';
 import ProjectsPage from './pages/ProjectsPage';
+import FolderPage from './pages/FolderPage';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MetaTitleTool />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/Folder" element={<FolderPage />} />
+        
       </Routes>
     </Router>
   );
